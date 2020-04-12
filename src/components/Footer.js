1c1e6d51a20d2416c73d21243b3940ea7b683ea0
Morgan Baker <morganbakerdesigner@gmail.com>
2020-04-12 11:53:39 +0100

    restyled footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,4 @@
 import React from "react"
-import links from "../constants/links"
-import socials from "../constants/socials"
-import AniLink from "gatsby-plugin-transition-link/AniLink"
 import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 
@@ -19,81 +16,11 @@ const GridContainer = styled.div`
   }
 `
 
-const FooterMenu = styled.div`
-  padding-top: 40px;
-  padding-bottom: 40px;
-  border-bottom: 1px solid var(--border);
-
-  @media (min-width: 768px) {
-    border-bottom: none;
-    padding-bottom: 0;
-  }
-
-  ul {
-    list-style: none;
-    display: flex;
-    flex-direction: column;
-    padding: 0;
-    margin: 0;
-  }
-
-  li {
-    text-transform: capitalize;
-    font-size: var(--menuItem);
-    font-weight: 900;
-    letter-spacing: -0.5px;
-    position: relative;
-    padding-bottom: 10px;
-    align-self: flex-start;
-
-    &:last-child {
-      padding-bottom: 5px;
-
-      &::after {
-        bottom: 4px;
-      }
-    }
-
-    &::after {
-      content: "";
-      display: block;
-      position: absolute;
-      height: 3px;
-      left: 0;
-      right: 0;
-      bottom: 8px;
-      background-color: var(--primary);
-    }
-
-    @media (min-width: 1200px) {
-      font-size: var(--menuItem);
-    }
-  }
-
-  a {
-    color: #fff;
-    text-decoration: none;
-    padding: 0;
-    transition: var(--transition) color;
-
-    &:focus {
-      color: var(--primary);
-    }
-  }
-
-  @media (hover: hover) {
-    a:hover {
-      color: var(--primary);
-    }
-  }
-`
-
 const Copyright = styled.div`
   padding-top: 20px;
   padding-bottom: 20px;
   font-size: 0.875rem;
   width: 100%;
-  border-top: 1px solid var(--border);
 
   a {
     text-decoration: none;
@@ -123,37 +50,6 @@ const Footer = () => {
     <>
       <FooterArea>
         <GridContainer className="container">
-          <FooterMenu>
-            <ul>
-              {links.map((item, index) => {
-                return (
-                  <li key={index}>
-                    <AniLink cover bg="#1d1d1d" to={item.path}>
-                      {item.text}
-                    </AniLink>
-                  </li>
-                )
-              })}
-            </ul>
-          </FooterMenu>
-          <FooterMenu>
-            <ul>
-              {socials.map((item, index) => {
-                return (
-                  <li>
-                    <a
-                      key={index}
-                      href={item.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {item.title}
-                    </a>
-                  </li>
-                )
-              })}
-            </ul>
-          </FooterMenu>
           <Copyright>
             Copyright &copy; {new Date().getFullYear()}{" "}
             <a
