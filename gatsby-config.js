5d1557550b8bd425325ccea5c2c570559d7fc8a9
Morgan Baker <morganbakerdesigner@gmail.com>
2020-10-15 08:10:48 +0100

    Updates to home layouts

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
     {
       resolve: `gatsby-v2-plugin-page-transitions`,
       options: {
-        transitionTime: 3500,
+        transitionTime: 2000,
       },
     },
     {
