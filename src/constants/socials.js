af568ed8051418802e23cc50331f09b326e2f993
beni dibatia <42493674+turzey@users.noreply.github.com>
2020-11-05 22:20:42 +0200

    Update social.js

--- src/constants/socials.js
+++ src/constants/socials.js
@@ -1,10 +1,10 @@
 export default [
   {
     title: "LinkedIn",
-    url: "https://www.linkedin.com/in/morgan-baker-developer-inverness/",
+    url: "https://www.linkedin.com/in/beni-dibatia-0ab7a7133/",
   },
   {
     title: "GitHub",
-    url: "https://github.com/bagseye",
+    url: "https://github.com/turzey",
   },
 ]
